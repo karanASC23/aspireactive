b042d9c527ee8c79ead5964e36ae51b4469efe79
karan11355@gmail.com <karan11355@gmail.com>
2023-08-16 10:34:34 -0400

    first commit

--- split.js
+++ split.js
@@ -1,31 +1,23 @@
-// const navLinks = document.querySelectorAll('nav a');
+const navLinks = document.querySelectorAll('nav a');
 
-// navLinks.forEach(link => {
-//     link.addEventListener('click', (event) => {
-//         event.preventDefault();
-//         const targetId = link.getAttribute('href').substring(1);
-//         const targetSection = document.getElementById(targetId);
-//         // targetSection.scrollIntoView({ behavior: 'smooth' });
-//     });
-// });
 
-// // Accordion functionality for training splits
-// const accordionItems = document.querySelectorAll('.accordion-item');
+// Accordion functionality for training splits
+const accordionItems = document.querySelectorAll('.accordion-item');
 
-// accordionItems.forEach(item => {
-//     const accordionHeader = item.querySelector('.accordion-header');
-//     const accordionContent = item.querySelector('.accordion-content');
+accordionItems.forEach(item => {
+    const accordionHeader = item.querySelector('.accordion-header');
+    const accordionContent = item.querySelector('.accordion-content');
 
-//     accordionHeader.addEventListener('click', () => {
-//         accordionContent.classList.toggle('active');
-//     });
-// });
+    accordionHeader.addEventListener('click', () => {
+        accordionContent.classList.toggle('active');
+    });
+});
 
-// const accordionHeaders = document.querySelectorAll('.accordion-header');
+const accordionHeaders = document.querySelectorAll('.accordion-header');
 
-// accordionHeaders.forEach(header => {
-//     header.addEventListener('click', () => {
-//         const content = header.nextElementSibling;
-//         content.style.display = content.style.display === 'block' ? 'none' : 'block';
-//     });
-// });
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    });
+});
